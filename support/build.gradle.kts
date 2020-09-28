6ef7ccdeebea0b8107af05136a88365b3c00e0f6
carlos quezada <carlos.quezada@ceiba.com.co>
2020-09-28 09:16:18 -0500

    Gral: Creacion Modulo Support

---
+++ support/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(appdependencies.Builds.COMPILE_VERSION)
+    buildToolsVersion = appdependencies.Builds.BUILD_TOOLS
+    defaultConfig {
+        minSdkVersion(appdependencies.Builds.MIN_VERSION)
+        targetSdkVersion(appdependencies.Builds.TARGET_VERSION)
+        versionCode = appdependencies.Builds.Core.VERSION_CODE
+        versionName = appdependencies.Builds.Core.VERSION_NAME
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+            isDebuggable = true
+        }
+
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+
+}
+
+dependencies {
+    implementation(fileTree(mapOf("include" to listOf("*.jar"), "dir" to "libs")))
+    implementation(kotlin("stdlib-jdk8", appdependencies.Versions.kotlin))
+
+
+    implementation("androidx.room:room-runtime:2.2.5")
+    kapt("androidx.room:room-compiler:2.2.5")
+    implementation("androidx.room:room-ktx:2.2.5")
+
+}
