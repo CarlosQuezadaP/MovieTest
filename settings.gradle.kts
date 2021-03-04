c7addb97e9c3bd5e817a384a8deeb0c4086308f8
Carlos Quezada <carlos.quezada@ceiba.com.co>
2021-03-03 23:51:00 -0500

    v1.0.0 Feature : Se agregan modulo adicionales data, domain, usecases

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,5 @@
+include(":usecases")
+include(":domain")
+include(":data")
 include(":app",":CORE",":infraestructura",":provide",":models",":home",":shoppingCart",":detailMovie")
 rootProject.name = "MerqueoPrueba"
