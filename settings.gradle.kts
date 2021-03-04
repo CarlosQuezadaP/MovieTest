2236713443de595ec17d4bc4382fe3d64bc1ad8d
Carlos Quezada <carlos.quezada@ceiba.com.co>
2021-03-04 01:29:01 -0500

    v1.0.0 Feature : Se elimina modulo DetailFragment, Shopping car , y se migra a modulo app

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 include(":usecases")
 include(":domain")
 include(":data")
-include(":app",":CORE",":infraestructura",":provide",":models",":home",":shoppingCart",":detailMovie")
+include(":app",":CORE",":infraestructura",":provide",":models",":home")
 rootProject.name = "MerqueoPrueba"
