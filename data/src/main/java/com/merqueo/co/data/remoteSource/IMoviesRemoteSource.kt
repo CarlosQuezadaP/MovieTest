262e60e471433fb8133c4d14a14f7d2035154dbc
Carlos Quezada <carlos.quezada@ceiba.com.co>
2021-03-06 09:08:08 -0500

    v1.0.0 Refactor: Se agrega repositorio base para consumir servicios

--- data/src/main/java/com/merqueo/co/data/remoteSource/IMoviesRemoteSource.kt
+++ data/src/main/java/com/merqueo/co/data/remoteSource/IMoviesRemoteSource.kt
@@ -1,8 +1,9 @@
 package com.merqueo.co.data.remoteSource
 
+import com.merqueo.co.domain.models.MovieItemDomain
 import kotlinx.coroutines.flow.Flow
 
 interface IMoviesRemoteSource {
 
-    suspend fun getUpcomingMovies(page: Int): Flow<List<com.merqueo.co.domain.models.MovieItemDomain>>
+    suspend fun getUpcomingMovies(page: Int): Flow<List<MovieItemDomain>>
 }
